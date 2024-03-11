659a81248abbc493c7517ed0632d86af60ce7382
Naka Rhythm <therapstar75@gmail.com>
2024-03-11 05:21:43 +0000

    sunan

--- deteksi.js
+++
@@ -1,7 +0,0 @@
-const detectLanguage = require('deteksi_bahasa');
-
-const textToDetect = 'Hello, how are you?';
-
-const detectedLanguage = detectLanguage.detect(textToDetect);
-
-console.log(Detected language: ${detectedLanguage});
